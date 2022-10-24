b51a2cf508ddbcb4a938ff414585b8f830661867
SalamunHasan69e <salamun2600@gmail.com>
2022-10-25 02:13:04 +0600

    added data

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
   res.send('Courses API Running')
 });
+
+app.get('/course', (req, res) => {
+  res.send(courses)
+})
 
 app.listen(port, () => {
   console.log('Courses server running on port');
