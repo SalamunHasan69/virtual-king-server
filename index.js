5d3e08e23435d7b14ac1b13d716d78d0d501f2fb
SalamunHasan69e <salamun2600@gmail.com>
2022-10-25 12:09:56 +0600

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const courses = require('./data/courses.json');
 
@@ -8,8 +11,10 @@ app.get('/', (req, res) => {
   res.send('Courses API Running')
 });
 
-
+app.get('/course', (req, res) => {
+  res.send(courses)
+});
 
 app.listen(port, () => {
   console.log('Courses server running on port');
-})
+});
