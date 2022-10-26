84847e0ac9ca959b2fa0f08cfa6ba3c6604686c2
SalamunHasan69e <salamun2600@gmail.com>
2022-10-26 14:44:06 +0600

    update server data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+// const course = require('./data/course.info.json');
 
 app.get('/', (req, res) => {
   res.send('Courses API Running')
@@ -14,6 +15,12 @@ app.get('/', (req, res) => {
 app.get('/course', (req, res) => {
   res.send(courses)
 });
+
+// app.get('/course-info/:id', (req, res) => {
+//   const id = req.params.id;
+//   const selectedCourse = course.find(c => c._id === id);
+//   res.send(selectedCourse);
+// })
 
 app.listen(port, () => {
   console.log('Courses server running on port');
